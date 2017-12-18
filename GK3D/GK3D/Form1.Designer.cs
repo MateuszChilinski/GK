479662b215335bd1176296107182e59d8bce5aef
Mateusz Chiliński <mateusz@chilinski.eu>
2017-12-18 15:58:10 +0100

    Added LookAt function(own implementation).

--- GK3D/GK3D/Form1.Designer.cs
+++ GK3D/GK3D/Form1.Designer.cs
@@ -29,24 +29,26 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1.Paint += PictureBox1_Paint;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(34, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(423, 377);
+            this.pictureBox1.Size = new System.Drawing.Size(1057, 765);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 643);
+            this.ClientSize = new System.Drawing.Size(1083, 791);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- GK3D/GK3D/Form1.cs
+++ GK3D/GK3D/Form1.cs
@@ -19,10 +19,7 @@ namespace GK3D
                    0, 1, 0, 0,
                    0, 0, 1, 0,
                    0, 0, 0, 1);
-        Matrix4x4 Mview = new Matrix4x4(-0.113546591F, 0.993532673F, 0, -0.099353267F,
-            0, 0, 1, -0.5F,
-            0.993532673F, 0.113546591F, 0F, -3.53413765F,
-            0, 0, 0, 1);
+        Matrix4x4 Mview = CreateViewAt(3.5, 0.5, 0.5, 0, 0.1, 0.5, 0, 0, 1);
         Matrix4x4 Mproj = new Matrix4x4(2.414213562F, 0, 0, 0,
                         0, 2.414213562F, 0, 0,
                         0, 0, -1.02020202F, -2.02020202F,
@@ -75,6 +72,39 @@ namespace GK3D
             c.addEdge(new Edge(new Vector4(0, 0, 1, 1), new Vector4(0, 1, 1, 1)));
             Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             pictureBox1.Image = bm;
+        }
+        static private Matrix4x4 CreateViewAt(double CPx, double CPy, double CPz, double CTx, double CTy, double CTz, double UPVx, double UPVy, double UPVz)
+        {
+            double zAxis_X = CPx - CTx;
+            double zAxis_Y = CPy - CTy;
+            double zAxis_Z = CPz - CTz;
+            double nom = Math.Sqrt(zAxis_X * zAxis_X + zAxis_Y * zAxis_Y + zAxis_Z * zAxis_Z);
+            double zAxis_XW = zAxis_X /nom;
+            double zAxis_YW = zAxis_Y / nom;
+            double zAxis_ZW = zAxis_Z / nom;
+
+            double xAxis_X = UPVy * zAxis_ZW - UPVz * zAxis_YW;
+            double xAxis_Y = UPVz * zAxis_XW - UPVx * zAxis_ZW;
+            double xAxis_Z = UPVx * zAxis_YW - UPVy * zAxis_XW;
+            nom = Math.Sqrt(xAxis_X * xAxis_X + xAxis_Y * xAxis_Y + xAxis_Z * xAxis_Z);
+            double xAxis_XW = xAxis_X / nom;
+            double xAxis_YW = xAxis_Y / nom;
+            double xAxis_ZW = xAxis_Z / nom;
+
+            double yAxis_X = zAxis_YW * xAxis_ZW - zAxis_ZW * xAxis_YW;
+            double yAxis_Y = zAxis_ZW * xAxis_XW - zAxis_XW * xAxis_ZW;
+            double yAxis_Z = zAxis_XW * xAxis_YW - zAxis_YW * xAxis_XW;
+            nom = Math.Sqrt(yAxis_X * yAxis_X + yAxis_Y * yAxis_Y + yAxis_Z * yAxis_Z);
+            double yAxis_XW = yAxis_X / nom;
+            double yAxis_YW = yAxis_Y / nom;
+            double yAxis_ZW = yAxis_Z / nom;
+            Matrix4x4 rtMatrix;
+            Matrix4x4.Invert(new Matrix4x4(
+                (float)xAxis_XW, (float)yAxis_XW, (float)zAxis_XW, (float)CPx,
+                (float)xAxis_YW, (float)yAxis_YW, (float)zAxis_YW, (float)CPy,
+                (float)xAxis_ZW, (float)yAxis_ZW, (float)zAxis_ZW, (float)CPz,
+                0, 0, 0, 1), out rtMatrix);
+            return rtMatrix;
         }
         public Vector4 CreatePoint(Vector4 point)
         {
